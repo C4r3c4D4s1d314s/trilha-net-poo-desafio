d95e74f90e48521dd2928c160e23012cab46a1df
Carlos E de O Franco <carloseofranco@proton.me>
2023-12-16 09:40:46 -0300

    Desadio entregue em 16/12/2023

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"16991234567", modelo: "Modelo Nk1", imei:"ab.cd.ef.gh.ij.kl", memoria:8);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("/n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero:"16920003736", modelo: "Ip12", imei:"ab.cd.ef.gh.ij.no", memoria:128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
